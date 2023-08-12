f1edd17b926165b14655ad7f9c109b66f0574870
samratbatra <samratbatra8@gmail.com>
2023-08-12 20:12:44 +0530

    Rabbit MQ producer and Consumer

--- customer/src/main/java/com/buddycode/customer/CustomerApplication.java
+++ customer/src/main/java/com/buddycode/customer/CustomerApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.buddycode.customer",
+                "com.buddycode.amqp"
+        }
+)
 @EnableEurekaClient
 @EnableFeignClients(
         basePackages = "com.buddycode.clients"
